0f15c3b9e01e4f8b23de7847a3597ea41f03989b
koala <1129269131@qq.com>
2021-01-29 10:13:35 +0800

    +添加SpringBoot-actuator学习笔记

---
+++ springboot-08-actuator/src/main/java/com/tjh/actuator/Springboot08ActuatorApplication.java
@@ -0,0 +1,16 @@
+package com.tjh.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 测试方法：http://localhost:8080/actuator
+ */
+@SpringBootApplication
+public class Springboot08ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot08ActuatorApplication.class, args);
+    }
+
+}
